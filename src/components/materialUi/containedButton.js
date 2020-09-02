86daeed2535511dc3fd0b6ce0fb4e82a01161358
Mila <camilasouza@apples-MacBook-Pro.local>
2020-09-02 14:53:59 -0300

    Componente do Footer + Footer no App.js

--- src/components/materialUi/containedButton.js
+++
@@ -1,7 +0,0 @@
-// Copiar e colar o código onde for utilizar.
-// Mudar o texto para o texto desejado.
-
-import React from 'react';
-import Button from '@material-ui/core/Button';
-
-<Button variant="contained" color="secondary">Texto</Button>
