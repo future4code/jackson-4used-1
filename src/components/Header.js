026a04541ac6deb023c5766758125b4721d45c81
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-09-02 19:45:43 -0300

    Adicionando Input e ShoppingCart do Material ao Header. Algumas alterações no estilo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,79 +1,90 @@
 import React from 'react'
 import styled from 'styled-components'
 import Logotipo from '../2.png'
-import Input from '@material-ui/core/Input';
-import Badge from '@material-ui/core/Badge';
-
+import { withStyles } from '@material-ui/core/styles'
+import Badge from '@material-ui/core/Badge'
+import IconButton from '@material-ui/core/IconButton'
+import FormControl from '@material-ui/core/FormControl'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 const All = styled.div`
  background-color: #FFFCEF;
  display: flex;
- justify-content: space-around;
+ justify-content: space-between;
+ align-items: center;
+ padding: 8px 24px;
+ margin: 0 24px;
  border-bottom: 1px solid #EEE9E9;
- font-family: 'Merriweather', serif;
+ /* font-family: 'Merriweather', serif; */
 `
+
 const Logo = styled.img`
     width: 50px;
-    height: 50px;
-    margin-left: 150px;
-    margin-right: 15px;
-    
+    height: 50px;   
 `
 const Container1 = styled.div`
     display: flex;
-    margin-top: 5px;
     justify-content: center;
     align-items: center;
-    margin-left: 50px;
-    
+    gap: 15px;
 `
 const Container2 = styled.div`
-    margin-right: 180px;
     align-self: center;
-    
 `
 const SpanStyled = styled.span`
     margin: 15px;
+    font-weight: 600;
+    cursor: pointer;
     &:hover {
         color: #F04E3E;
     }
 `
-const InputPersonalizade = styled.input`
-  border: none;
-  width: ${props => props.width};
-  height: 30px;
-  outline: none;
-  margin: 10px 2px 10px 3px;
-  border-radius: 5px;
-  width: 200px;
-  align-self: center;
-  border-bottom: 2px solid black;
-  background-color: #FFFCEF;
-  
-  `
 
-export default class Header extends React.Component {
+const StyledBadge = withStyles((theme) => ({
+    badge: {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}))(Badge)
 
-    
+export default class Header extends React.Component {    
     render() {
         return (
             <All>
                 <Container1>
                     <Logo src={Logotipo} />
-                    <InputPersonalizade placeholder="Buscar.." />
+                    <FormControl variant="outlined">
+                        <OutlinedInput 
+                            id="component-outlined" 
+                            placeholder= "busca..."
+                            margin="dense"
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton size="small">
+                                        <SearchIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
                 </Container1>
                 <Container2>
                     <SpanStyled>Produtos</SpanStyled>
                     <SpanStyled>Entrar</SpanStyled>
                     <SpanStyled>Quero vender</SpanStyled>
                     <SpanStyled>Fale conosco</SpanStyled>
-                    <SpanStyled>Carrinho</SpanStyled>
+                    <IconButton aria-label="shopping cart" component="span"> {/*Se houver problemas com a cor, mudar para secondary*/}
+                        <StyledBadge badgeContent={3} color="secondary">
+                            <ShoppingCartIcon />
+                        </StyledBadge>
+                    </IconButton>
                 </Container2>
             </All>
-         
-        )
-         
-    
-        
+        )    
     }
 }
