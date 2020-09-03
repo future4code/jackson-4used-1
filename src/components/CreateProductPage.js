300266308f8b13853b8a24fac896f1a78b37439b
Gregory Mendes <gregory.gustavo1997@gmail.com>
2020-09-03 15:17:18 -0300

    Atualizando o formulário.

--- src/components/CreateProductPage.js
+++ src/components/CreateProductPage.js
@@ -3,16 +3,24 @@ import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select'
 import Button from '@material-ui/core/Button';
 import { styled } from '@material-ui/styles'
-import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles';
+import { InputLabel, FormControl } from '@material-ui/core';
 
-const FormDiv = styled(Box)({
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    }
+}))
+
+const FormDiv = styled(Grid)({
     backgroundColor: "#FFFCEF",
-    flexDirection: "column",
-    display: "flex",
+    display: "grid",
     alignItems: "center",
-    justifyContent: "space-evenly",
+    justifyItems: "center",
     width: "100vw",
     height: "100vh",
+    marginBottom: "50px"
 })
 
 const InputField = styled(TextField)({
@@ -32,39 +40,52 @@ const DescriptionInputField = styled(TextField)({
 
 const SelectOption = styled(Select)({
     width: "30vw",
-    backgroundColor: "#FFFFFF"
+    backgroundColor: "#FFFFFF",
 })
 
 
 export default function CreateProductPage() {
 
+    const classes = useStyles();
+
     return (
-        <div>
-            <FormDiv >
+        <div className={classes.root}>
+            <FormDiv container >
+                <h2>Cadastro de produto</h2>
                 <NamesInputField variant="outlined" placeholder="Nome"/>
                 <NamesInputField variant="outlined" placeholder="Sobrenome"/>
                 <InputField variant="outlined" placeholder="Nome do produto"/>
-                <SelectOption
-                    native
-                    variant="outlined"
-                >
-                    <option value=""/>
-                    <option>Instrumentos</option>
-                    <option>Mídia</option>
-                    <option>Colecionáveis</option>
-                </SelectOption>  
+                <FormControl variant="outlined">
+                    <InputLabel id="category-label">Categoria</InputLabel>
+                    <SelectOption
+                        labelId="category-label"
+                        label="Categoria"
+                        native
+                        variant="outlined"
+                    >
+                        <option value=""/>
+                        <option>Instrumentos</option>
+                        <option>Mídia</option>
+                        <option>Colecionáveis</option>
+                    </SelectOption>  
+                </FormControl>
                 <DescriptionInputField variant="outlined" multiline rows={10} placeholder={"Descrição"}/>
                 <InputField variant="outlined" placeholder={"Link de imagem do produto (URL)"}/>
                 <div>Imagem entrará aqui</div>
-                <SelectOption 
-                    native
-                    variant="outlined"
-                >
-                    <option value=""/>
-                    <option>Cartão</option>
-                    <option>Boleto</option>
-                    <option>Transferência</option>
-                </SelectOption>
+                <FormControl variant="outlined">
+                    <InputLabel id="form-of-payment-label">Forma de pagamento</InputLabel>
+                    <SelectOption
+                        labelId="form-of-payment-label"
+                        label="Forma de pagamento"
+                        native
+                        variant="outlined"
+                    >
+                        <option value=""/>
+                        <option>Cartão</option>
+                        <option>Boleto</option>
+                        <option>Transferência</option>
+                    </SelectOption>
+                </FormControl>
                 <Button variant="contained" color="secondary">Enviar</Button>
             </FormDiv>
         </div>
